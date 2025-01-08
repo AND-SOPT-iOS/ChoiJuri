28a1cc72df14d4afafbe74caa6e68a6e7b39cef8
JURI <80569323+juri123123@users.noreply.github.com>
2025-01-08 15:04:04 +0900

    feat: mvi 구조에 맞게 domain 정의

---
+++ ConcurrencyPractice/ConcurrencyPractice/Feature/BoxOfficeDomain.swift
@@ -0,0 +1,29 @@
+//
+//  BoxOfficeDomain.swift
+//  ConcurrencyPractice
+//
+//  Created by 최주리 on 1/8/25.
+//
+
+import Foundation
+
+// viewModel에서 published로 선언하는 변수 느낌이라고 이해함
+struct BoxOfficeState {
+    var movies: [BoxOffice] = []
+    var movie: Movie? = nil
+    var isLoading: Bool = false
+    var error: String? = nil
+}
+
+// 사용자가 할 수 있는 행동의 모든 경우의 수 (action)
+enum BoxOfficeIntent {
+    case onAppear
+    case movieSelected(String)
+}
+
+// 사용자의 행동에 따른 비즈니스 로직?과 발생할 수 있는 효과?
+enum BoxOfficeEffect {
+    case fetchBoxOffices
+    case fetchMovie(Int)
+    case showError(String)
+}
---
+++ ConcurrencyPractice/ConcurrencyPractice/Feature/BoxOfficeStore.swift
@@ -0,0 +1,62 @@
+//
+//  BoxOfficeStore.swift
+//  ConcurrencyPractice
+//
+//  Created by 최주리 on 1/8/25.
+//
+
+import Foundation
+
+// 어쩌면 뷰모델이랑 비슷한 역할인데
+// 변수 부분을 state로 관리하고
+// 함수 부분을 intent로 관리하는 느낌이랄까 ? ? ? 
+final class BoxOfficeStore: ObservableObject {
+    
+    @Published private(set) var state: BoxOfficeState
+    private let service: BoxOfficeService
+    
+    init(
+        state: BoxOfficeState = BoxOfficeState(),
+        service: BoxOfficeService = BoxOfficeService()
+    ) {
+        self.state = state
+        self.service = service
+    }
+
+    func dispatch(_ intent: BoxOfficeIntent) {
+        switch intent {
+        case .onAppear:
+            Task {
+                do {
+                    state.isLoading = true
+                    
+                    state.movies = try await service
+                        .fetchBoxOfficeList()
+                        .boxOfficeResult
+                        .dailyBoxOfficeList
+                    
+                    state.isLoading = false
+                } catch {
+                    state.error = error.localizedDescription
+                }
+            }
+        case .movieSelected(let code):
+            Task {
+                do {
+                    state.isLoading = true
+                    
+                    state.movie = try await service
+                        .fetchMovieInfo(code: code)
+                        .movieInfoResult
+                        .movieInfo
+                    
+                    state.isLoading = false
+                } catch {
+                    state.error = error.localizedDescription
+                }
+            }
+        }
+    }
+    
+    
+}
